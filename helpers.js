8fd7b2f98c5e8c21b651670ff5cd4741d0db3e3c
rauljr7 <doyouguysspam@gmail.com>
2024-04-05 00:20:21 -0700

    s

--- helpers.js
+++ helpers.js
@@ -85,7 +85,7 @@ document.onclick = function(event) {
         event.target.style.display = "none";
     } else
     if (event.target.id === "donate") {
-        build_payment_link_url.label = "donate";//
+        payment_link.label = "donate";//
         window.location.href = build_payment_link_url();
     }
     if (event.target.classList.contains("modal")) {
